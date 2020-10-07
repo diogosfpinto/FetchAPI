c69817277f63a50b52bfd7933782e736b3e0c551
diogosfpinto <diogo.sfpinto@gmail.com>
2020-10-06 21:57:43 -0400

    created method cadastrarClientes with method fetch POST

--- api/cliente.js
+++ api/cliente.js
@@ -1,4 +1,4 @@
-listarClientes = () => {
+const listarClientes = () => {
     return fetch('http://localhost:4000/clientes')
     .then(resposta => {
         return resposta.json()
@@ -6,5 +6,23 @@ listarClientes = () => {
     .then( json => {
         return json;
     })
+}
+
+const cadastrarClientes = (nome, cpf) => {
+    const json = JSON.stringify({
+        nome: nome,
+        cpf: cpf
+    })
+    
+    return fetch('http://localhost:4000/clientes/cliente', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: json
+    })
+    .then( resp => {
+        return resp.body;
+    }) 
 }
 
